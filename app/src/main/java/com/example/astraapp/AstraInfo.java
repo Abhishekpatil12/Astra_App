83f33bc8170e2904bc4a86b22c804e23448e2a72
Om Bhavsar <bhavsarom007@gmail.com>
2024-01-18 11:12:21 +0530

    Commit Om

---
+++ app/src/main/java/com/example/astraapp/AstraInfo.java
@@ -0,0 +1,14 @@
+package com.example.astraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AstraInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_astra_info);
+    }
+}
