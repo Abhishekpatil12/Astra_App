dcb728a404a7bab84faee5806b7102fb38ed40b8
Abhishekpatil12 <patilabhishek191@gmail.com>
2024-09-22 00:21:23 +0530

    abhi commit15

--- app/src/main/java/com/example/astraapp/activities/Donate.java
+++ app/src/main/java/com/example/astraapp/activities/Donate.java
@@ -1,8 +1,9 @@
 package com.example.astraapp.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,9 +13,23 @@ import com.example.astraapp.R;
 
 public class Donate extends AppCompatActivity {
 
+    ImageButton btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
+        btn =  findViewById(R.id.buttonback);
+
+        onclickfunc();
+    }
+
+    private void onclickfunc() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
